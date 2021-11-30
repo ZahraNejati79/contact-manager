a6808e201ccdc0aad141f86040df9ebf43f2a4a2
ZahraNejati <nejatizahra4@gmail.com>
2021-11-30 22:19:10 +0330

    contact component

---
+++ src/components/ContactList/Contact/Contact.jsx
@@ -0,0 +1,27 @@
+import { FaUser } from "react-icons/fa";
+import { FiTrash2 } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import styles from "../ContactList.module.css";
+const Contact = ({ contact }) => {
+  const id = contact.id;
+  return (
+    <div key={id} className={styles.contactTrash}>
+      <div className={styles.contact}>
+        <div className={styles.icon}>
+          <FaUser />
+        </div>
+        <Link to={`usr/:${id}`}>
+          <div className={styles.nameEmail}>
+            <span>{contact.Name}</span>
+            <span>{contact.Email}</span>
+          </div>
+        </Link>
+      </div>
+      <button className={styles.trash}>
+        <FiTrash2 />
+      </button>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,12 @@
 import styles from "./ContactList.module.css";
 import { FaUser } from "react-icons/fa";
 import { FiTrash2 } from "react-icons/fi";
+import Contact from "./Contact/Contact";
 const ContactList = ({ contacts }) => {
   return (
     <div className={styles.map}>
       {contacts.map((contact) => (
-        <div className={styles.contactTrash}>
-          <div className={styles.contact}>
-            <div className={styles.icon}>
-              <FaUser />
-            </div>
-            <div className={styles.nameEmail}>
-              <span>{contact.Name}</span>
-              <span>{contact.Email}</span>
-            </div>
-          </div>
-          <div className={styles.trash}>
-            <FiTrash2 />
-          </div>
-        </div>
+        <Contact contact={contact} />
       ))}
     </div>
   );
